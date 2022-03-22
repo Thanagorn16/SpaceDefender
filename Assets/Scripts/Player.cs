ed8b49e04174ee3755eb18f1f8f341d9f7aab95c
Thanagorn16 <thanagorn1611@gmail.com>
2022-03-22 14:15:40 +0700

    add player shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,17 @@ public class Player : MonoBehaviour
     Vector2 rawInput;
     Vector2 minBounds;
     Vector2 maxBounds;
+    Shooter shooter;
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float paddingLeft;
     [SerializeField] float paddingRight;
     [SerializeField] float paddingTop;
     [SerializeField] float paddingBottom;
+
+    void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+    }
     
     void Start()
     {
@@ -45,5 +51,13 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value)
+    {
+        if(shooter != null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
     }
 }
